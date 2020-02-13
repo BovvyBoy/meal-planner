edb9f3cd47247e2e14e46dfd8dd9e48fcd15f187
Daniel  Bovington <dbovington@MacBook-Pro-2.home>
2020-02-13 19:22:19 +0000

    Working on api population

--- meal-planner-api/db/seeds.rb
+++ meal-planner-api/db/seeds.rb
@@ -1,3 +1,5 @@
+require ‘rest-client’
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -13,3 +15,10 @@ weekshop = Planner.create(user_id: 1, name: "Weekly Shop", duration: 7)
 lasagna = Recipe.create(name: "Lasagne", origin: "Italy", instructions: "please cook", image: "http://imgsource.com", video: "http://vidsource.com", category: "Pasta")
 
 danweekshop = PlannerRecipe.create(recipe_id: 1, planner_id: 1)
+
+
+meal_cats = ["Beef", "Breakfast", "Chicken", "Dessert", "Goat", "Lamb", "Miscellaneous", "Pasta", "Pork", "Seafood", "Side", "Starter", "Vegan", "Vegetarian"]
+
+meal_cats.each do |cat|
+    RestClient.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=#{cat}") 
+end
