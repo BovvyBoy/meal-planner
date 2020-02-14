3ce0aff51376df07a1dfb9520bebc1705b493e74
Daniel  Bovington <dbovington@MacBook-Pro-2.home>
2020-02-14 20:04:06 +0000

    created jwt controllers and models with the config

--- meal-planner-api/app/models/user.rb
+++ meal-planner-api/app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
+  has_many :planners
+  has_many :recipe_planners, through: :planners
+  has_many :recipes, through:, :recipe_planners  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+
+  devise :database_authenticatable, 
+         :jwt_authenticatable,
+         :registerable,
+         jwt_revocation_strategy: JwtBlacklist
+
+
 end
--- meal-planner-api/config/routes.rb
+++ meal-planner-api/config/routes.rb
@@ -7,7 +7,18 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:create]
+      # resources :users, only: [:create]
+      devise_for :users,
+                 path: '',
+                 path_names: {
+                   sign_in: 'login',
+                   sign_out: 'logout',
+                   registration: 'signup'
+                 },
+                 controllers: {
+                   sessions: 'sessions',
+                   registrations: 'registrations'
+                 }
     end
   end
 
