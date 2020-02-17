e65237e9372142cc80cb75f8c213c894f7c834ef
Daniel  Bovington <dbovington@MacBook-Pro-2.home>
2020-02-17 19:32:10 +0000

    just some cleanup on files

--- meal-planner-api/app/controllers/api/v1/recipes_controller.rb
+++ meal-planner-api/app/controllers/api/v1/recipes_controller.rb
@@ -1,14 +1,14 @@
 class Api::V1::RecipesController < ApplicationController
 
     def index
-        @recipes = Recipe.all
+        recipes = Recipe.all
 
-        render json: @recipes
+        render json: recipes
     end
 
     def show
-        @recipe = Recipe.find(params[:id])
+        recipe = Recipe.find(params[:id])
 
-        render json: @recipe
+        render json: recipe
     end
 end
