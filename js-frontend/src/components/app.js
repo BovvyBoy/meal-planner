6eb4b0d4b8898378f64345d9523741d514d52f36
Daniel  Bovington <dbovington@macbook-pro-2.home>
2020-02-18 21:52:25 +0000

    signup page

--- js-frontend/src/components/app.js
+++ js-frontend/src/components/app.js
@@ -1,11 +1,12 @@
 class App {
     constructor(){
         this.initBindingsAndEventListeners()
-        this.renderPage(new signupPage(this.pageContainer))
+        this.renderPage(new SignupPage(this.pageContainer))
     }
 
     initBindingsAndEventListeners(){
         this.container = document.querySelector('#app-container')
+        this.alertContainer = document.querySelector('#alerts-container')
         this.navbarContainer = document.querySelector('#navbar-container')
         this.pageContainer = document.querySelector('#page-container')
     }
--- js-frontend/src/pageManagers/signup.js
+++ js-frontend/src/pageManagers/signup.js
@@ -1,3 +1,36 @@
-class SignupPage(){
+class SignupPage{
 
+    constructor(container){
+        this.container = container
+    }
+
+    get staticHTML(){
+        return (`
+        <h1>Sign Up</h1>
+                
+        <form>
+            <div class="form-group">
+                <label for="name">Your Name</label>
+                <input type="text" class="form-control" id="name" placeholder="Please Enter Your Name">
+            </div>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" id="username" placeholder="Please Enter a username">
+            </div>
+            <div class="form-group">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" id="email" placeholder="Please Enter email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="And a Password">
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>`)
+    }
+
+    render(){
+        this.container.innerHTML = this.staticHTML
+    }
 }
