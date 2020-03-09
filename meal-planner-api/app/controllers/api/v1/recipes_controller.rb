1745e59b16444c7ff211f6610e30e5326eede61c
Daniel  Bovington <dbovington@macbook-pro-2.home>
2020-03-09 17:51:37 +0000

    fixes

--- meal-planner-api/app/controllers/api/v1/recipes_controller.rb
+++ meal-planner-api/app/controllers/api/v1/recipes_controller.rb
@@ -13,9 +13,12 @@ class Api::V1::RecipesController < ApplicationController
     end
 
     def create
-        planner_recipe = PlannerRecipe.new(planner_recipe_params)
-        planner_recipe.save
-        render_resource(planner_recipe)
+        # planner_recipe = PlannerRecipe.new(planner_recipe_params)
+        # planner_recipe.save
+        current_planner = Planner.find_by_id(params[:planner_id].to_i)
+        current_recipe = Recipe.find_by_id(params[:planner_recipe][:recipe_id].to_i)
+        planner_recipe = current_planner.recipes<<(current_recipe)
+        render_resource(current_planner)
     end
 
     private
